a6916bd4ca4ba0affd42e9befcaa4093abfeea5f
Chris Cheong <chrisxcheong@gmail.com>
2019-07-16 07:14:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {["Eyes"] "White"}
+  my_hash = {"Card" => "Blue-Eyes White Dragon"}
 
 
 end
